c7e2bcc919eaab2d205d6b8f161e922cbbfedc2e
dennisrein <dennis.rein@outlook.com>
2020-10-08 17:08:25 +0200

    Update .gitignore

--- hangouts-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\denni\dev\workspace\python\hangouts_to_pdf\hangouts-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
